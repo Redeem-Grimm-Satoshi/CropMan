15980da46311d4853ce5c68960d5ac08928301d8
Redeem-Grimm-Satoshi <45304978+Redeem-Grimm-Satoshi@users.noreply.github.com>
2023-10-07 11:33:21 +0100

    Save button added.. lil ui fixes

--- src/main/java/com/zolix/cropman/Controller/CropManController.java
+++ src/main/java/com/zolix/cropman/Controller/CropManController.java
@@ -4,11 +4,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-import java.io.File;
+import java.io.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,6 +21,9 @@ public class CropManController {
 
     @FXML
     private ImageView viewImage;
+
+    @FXML
+    private Button saveImage;
 
     @FXML
     private Label text;
@@ -35,7 +40,34 @@ public class CropManController {
             imageChooser.setTitle("Open Image");
             FileChooser.ExtensionFilter exfilterJPG=new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
             imageChooser.getExtensionFilters().addAll(exfilterJPG);
+
+            //show file dialog
             File file=imageChooser.showOpenDialog(new Stage());
+
+            //paste image to imageviewer
+            try {
+                InputStream inputStream=new FileInputStream(file);
+                Image image=new Image(inputStream);
+                viewImage.setImage(image);
+
+                //set image dimension while also preserving aspect ratio
+                viewImage.setFitHeight(600);
+                viewImage.setFitWidth(600);
+                viewImage.setPreserveRatio(true);
+                viewImage.setSmooth(true);
+                viewImage.setCache(true);
+            }catch (Exception e){
+
+            }
+        });
+
+        //save image event
+        saveImage.setOnAction(event -> {
+            //file saver with .jpg extension filter
+            imageChooser.setTitle("Save Image");
+            FileChooser.ExtensionFilter exfilterJPG=new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+            imageChooser.getExtensionFilters().addAll(exfilterJPG);
+            File save=imageChooser.showSaveDialog(new Stage());
         });
 
 
